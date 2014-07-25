07a0ee707f0d8a941fa09620e32fe975aecd3aff
Daniel Rothenberg <darothen@mit.edu>
2014-07-25 14:02:46 -0400

    Revamped interface to assimulo; can now use either CVODE or LSODAR, and the `terminate` flag correctly works for these methods.

--- parcel_model/integrator.py
+++ parcel_model/integrator.py
@@ -15,14 +15,16 @@ except ImportError:
 try: 
     from assimulo.problem import Explicit_Problem
     from assimulo.solvers.sundials import CVode, CVodeError
+    from assimulo.solvers.odepack import LSODAR
     from assimulo.exception import TimeLimitExceeded
-    available_integrators.extend(['cvode'])
+    available_integrators.extend(['cvode', 'lsodar'])
 except ImportError:
     print "Could not import Assimulo; invoking the CVode solver will fail!"
     pass
 
+from functools import partial
 from scipy.integrate import odeint
-from numpy import zeros
+import numpy as np
 
 class Integrator(object):
     """
@@ -42,7 +44,10 @@ class Integrator(object):
             'lsoda': Integrator._solve_lsoda,
             'lsode': Integrator._solve_lsode,
             'vode': Integrator._solve_vode,
-            'cvode': Integrator._solve_cvode,
+            #'cvode': Integrator._solve_cvode,
+            #'lsodar': Integrator._solve_lsodar,
+            'cvode': partial(Integrator._solve_with_assimulo, method='cvode'),
+            'lsodar': partial(Integrator._solve_with_assimulo, method='lsodar'),
         }
 
         if method in available_integrators:
@@ -136,33 +141,93 @@ class Integrator(object):
         return x, t, True
 
     @staticmethod
-    def _solve_cvode(f, t, y0, args, console=False, max_steps=1000, terminate=False, 
-                     **kwargs):
-        """Wrapper for Assimulo's CVODE-Sundials routine
+    def _solve_with_assimulo(f, t, y0, args, console=False, max_steps=1000, terminate=False, 
+                             method='cvode', **kwargs):
+        """ Wrapper for Assimulo's solver routines
         """
-        def rhs(t, u):
-            return f(u, t, *args)
+        def user_rhs(t, y):
+            dode_dt = f(y, t, *args)
+            return dode_dt
+
+        class Extended_Problem(Explicit_Problem):
+
+            name  = 'Parcel model ODEs'
+            sw0   = [True,  # Normal integration switch
+                     False, # Past cut-off switch
+            ] 
+            t_cutoff = 1e5
+            dS_dt = 1.0
+
+            def rhs(self, t, y, sw):
+                if not sw[1]: # Normal integration before cutoff
+                    dode_dt = f(y, t, *args)
+                    self.dS_dt = dode_dt[5]
+                else:
+                    dode_dt = np.zeros(6 + args[0])
+                return dode_dt
+
+            # The event function
+            def state_events(self, t, y, sw):
+                """ Check whether an 'event' has occurred. We want to see if the
+                supersaturation is decreasing or not. """
+                if sw[0]: 
+                    #dode_dt = f(y, t, *args)
+                    #smax_event = dode_dt[5]
+                    smax_event = self.dS_dt
+                else:
+                    smax_event = -1.0
+
+                t_cutoff_event = t - self.t_cutoff
+
+                return np.array([smax_event, t_cutoff_event])
+
+            # Event handling function
+            def handle_event(self, solver, event_info):
+                """ Event handling. This function is called when Assimulo finds
+                an event as specified by the event function. """
+                event_info = event_info[0] # Only state events, event_info[1] is time events
+                if event_info[0] != 0:
+                    solver.sw[0] = False
+                    self.t_cutoff = solver.t + 5.0
+
+            def handle_result(self, solver, t, y):
+                if t < self.t_cutoff:
+                    Explicit_Problem.handle_result(self, solver, t, y)
 
         ## Setup solver
-        prob = Explicit_Problem(rhs, y0)
-        sim = CVode(prob)
-        sim.discr = 'BDF'
-        sim.maxord = 5 
-        sim.maxsteps = max_steps
-
-        if "iter" in kwargs:
-            sim.iter = kwargs['iter']
+        if terminate:
+            prob = Extended_Problem(y0=y0)
         else:
-            sim.iter = 'Newton'
+            prob = Explicit_Problem(user_rhs, y0)
+
+        ## Choose simulator
+        if method == "cvode":
+            sim = CVode(prob)
+            sim.discr = 'BDF'
+            sim.maxord = 5 
+
+            if "iter" in kwargs:
+                sim.iter = kwargs['iter']
+            else:
+                sim.iter = 'Newton'
+
+            if "linear_solver" in kwargs:
+                sim.linear_solver = kwargs['linear_solver']
+
+        elif method == "lsodar":
+            sim = LSODAR(prob)
+            sim.maxords = 5
+
+        else:
+            raise ValueError("Passed method (%r) must be 'cvode' or 'lsodar'" % method)
+
+        sim.maxsteps = max_steps
 
         if "time_limit" in kwargs:
             sim.time_limit = kwargs['time_limit']
             sim.report_continuously = True
         else:
             sim.time_limit = 0.0
-
-        if "linear_solver" in kwargs:
-            sim.linear_solver = kwargs['linear_solver']
 
         ## Setup tolerances
         nr = args[0]
@@ -183,7 +248,7 @@ class Integrator(object):
             t, x = sim.simulate(t_end, steps)
         except CVodeError, e:
             raise ValueError("Something broke in CVode: %r" % e)
-            return None, None,False
+            return None, None, False
         except TimeLimitExceeded, e:
             raise ValueError("CVode took too long to complete")
             return None, None, False
