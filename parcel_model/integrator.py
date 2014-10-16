Removed P from state array; corrected wv0 calculation to correctly compute initial relative humidity from initial supersaturation

parcel_model/integrator.py
@@ -26,6 +26,9 @@ from functools import partial
 from scipy.integrate import odeint
 import numpy as np
 
+state_atol = [1e-4, 1e-4, 1e-10, 1e-10, 1e-8]
+state_rtol = 1e-7
+
 class Integrator(object):
     """
     Container class for the various integrators to use in the parcel model.
@@ -63,8 +66,8 @@ class Integrator(object):
         """Wrapper for odespy.odepack.Lsode
         """
         nr = args[0]
-        atol = [1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-8] + [1e-12]*nr
-        rtol = 1e-7
+        atol = state_atol + [1e-12]*nr
+        rtol = state_rtol
         kwargs = { 'atol': atol, 'rtol': rtol, 'nsteps':max_steps }
         f_w_args = lambda u, t: f(u, t, *args)
         f_terminate = lambda u, t, step_no: u[step_no][5] < u[step_no -1][5]
@@ -89,8 +92,6 @@ class Integrator(object):
         """Wrapper for odespy.odepack.Lsoda
         """
         nr = args[0]
-        atol = [1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-8] + [1e-12]*nr
-        rtol = 1e-7
 
         kwargs = { 'atol': atol, 'rtol': rtol, 'nsteps':max_steps }
         f_w_args = lambda u, t: f(u, t, *args)
@@ -116,8 +117,8 @@ class Integrator(object):
         """Wrapper for odespy.Vode
         """
         nr = args[0]
-        atol = [1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-8] + [1e-12]*nr
-        rtol = 1e-7
+        atol = state_atol + [1e-12]*nr
+        rtol = state_rtol
 
         #kwargs = { 'atol':1e-10, 'rtol':1e-8, 'nsteps':max_steps,
         #           'adams_or_bdf': 'bdf',  'order':5}
@@ -232,8 +233,8 @@ class Integrator(object):
         ## Setup tolerances
         nr = args[0]
         #ny = 6 + nr # z, P, T, wv, wc, S, *droplet_sizes        
-        sim.rtol = 1e-7
-        sim.atol = [1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-8] + [1e-12]*nr
+        sim.rtol = state_rtol
+        sim.atol = state_atol + [1e-12]*nr
 
         if not console:
             sim.verbosity = 50
@@ -276,8 +277,8 @@ class Integrator(object):
         """Wrapper for scipy.integrate.odeint
         """
         nr = args[0]
-        atol = [1e-4, 1e-4, 1e-4, 1e-10, 1e-10, 1e-8] + [1e-12]*nr
-        rtol = 1e-7
+        atol = state_atol + [1e-12]*nr
+        rtol = state_rtol
 
         x, info = odeint(f, y0, t, args=args, full_output=1, mxhnil=0,
                          mxstep=max_steps, atol=atol, rtol=rtol)

parcel_model/parcel.py
@@ -294,7 +294,7 @@ class ParcelModel(object):
 
         ## 2) Setup parcel initial conditions
         # a) water vapor
-        wv0 = (1.-S0)*0.622*es(T0-273.15)/(P0-es(T0-273.15)) # Water Vapor mixing ratio, kg/kg
+        wv0 = (S0 + 1.)*0.622* es(T0-273.15)/(P0-es(T0-273.15)) # Water Vapor mixing ratio, kg/kg
 
         # b) find equilibrium wet particle radius
         # wrapper function for quickly computing deviation from chosen
@@ -334,7 +334,7 @@ class ParcelModel(object):
         wc0 = np.sum([(4.*np.pi/3.)*rho_w*Ni*(r0**3 - r_dry**3) for r0, r_dry, Ni in zip(r0s, r_drys, Nis)])
 
         # d) concatenate into initial conditions arrays
-        y0 = [z0, P0, T0, wv0, wc0, S0]
+        y0 = [z0, T0, wv0, wc0, S0]
         if self.console:
             print "PARCEL INITIAL CONDITIONS"
             print "    {:>8} {:>8} {:>8} {:>8} {:>8}".format("P (hPa)", "T (K)", "wv", "wc", "S")
@@ -722,12 +722,11 @@ class ParcelModel(object):
 
         """
         z  = y[0]
-        P  = y[1]
-        T  = y[2]
-        wv = y[3]
-        wc = y[4]
-        S  = y[5]
-        rs = np.asarray(y[6:])
+        T  = y[1]
+        wv = y[2]
+        wc = y[3]
+        S  = y[4]
+        rs = np.asarray(y[5:])
 
         T_c = T - 273.15   # convert temperature to Celsius
         pv_sat = es(T-T_c) # saturation vapor pressure