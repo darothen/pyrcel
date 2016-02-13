597635e42a009bbb56531a80b768bc7c361ee81a
Daniel Rothenberg <darothen@mit.edu>
2016-02-12 23:28:37 -0500

    Re-structure setup.py and versioning schema

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
 #! /usr/bin/env python
-#
-# Copyright (C) 2015 Daniel Rothenberg <darothen@mit.edu>
-
-DESCRIPTION = "parcel_model: 0D adiabatic cloud parcel model"
-LONG_DESCRIPTION = """\
-This code implements a relatively simple, adiabatic cloud parcel model for studying aerosol
-activation. It allows the user to peform and iterate parcel model experiments in a simple 
-fashion through the use of an object-oriented implementation. Furthermore, interfaces for
-several numerical solvers are built into the model so that users can choose whatever scheme
-they would like to run with the model.
-"""
-
-DISTNAME = "parcel_model"
-MAINTAINER = "Daniel Rothenberg"
-AUTHOR = MAINTAINER
-MAINTAINER_EMAIL = "darothen@mit.edu"
-AUTHOR_EMAIL = MAINTAINER_EMAIL
-URL = "https://github.com/darothen/parcel_model"
-LICENSE = "New BSD (3-clause)"
-DOWNLOAD_URL = "https://github.com/darothen/parcel_model"
-
-execfile("parcel_model/version.py")
-VERSION = __version__
 
 try:
     from setuptools import setup
@@ -33,6 +10,33 @@ except ImportError:
 from Cython.Distutils import build_ext
 from distutils.extension import Extension
 import numpy
+
+import os
+import warnings
+from textwrap import dedent
+
+MAJOR, MINOR, MICRO = 1, 2, 0
+DEV = True
+VERSION = "{}.{}.{}".format(MAJOR, MINOR, MICRO)
+
+# Correct versioning with git info if DEV
+if DEV:
+    import subprocess
+
+    pipe = subprocess.Popen(
+        ['git', "describe", "--always", "--match", "v[0-9]*"],
+        stdout=subprocess.PIPE)
+    so, err = pipe.communicate()
+
+    if pipe.returncode != 0:
+        # no git or something wrong with git (not in dir?)
+        warnings.warn("WARNING: Couldn't identify git revision, using generic version string")
+        VERSION += ".dev"
+    else:
+        git_rev = so.strip()
+        git_rev = git_rev.decode('ascii') # necessary for Python >= 3
+
+        VERSION += ".dev-{}".format(git_rev)
 
 extensions = [
     Extension("parcel_model.parcel_aux", 
@@ -48,36 +52,57 @@ extensions = [
    #          ),
 ]
 
-if __name__ == "__main__":
+def _write_version_file():
 
-    setup(
-        name = DISTNAME,
-        author = AUTHOR,
-        author_email = AUTHOR_EMAIL,
-        maintainer = MAINTAINER,
-        maintainer_email = MAINTAINER_EMAIL,
-        description = DESCRIPTION,
-        long_description = LONG_DESCRIPTION,
-        license = LICENSE,
-        url = URL,
-        version = VERSION,
-        download_url = DOWNLOAD_URL,
+    fn = os.path.join(os.path.dirname(__file__), 'parcel_model', 'version.py')
 
-        install_requires = ['numpy', 'scipy', 'pandas', ],
-        packages = ["parcel_model", ],
-        ext_modules = extensions,
-        cmdclass = {'build_ext': build_ext},
+    version_str = dedent("""
+        __version__ = '{}'
+        """)
 
-        classifiers=[
-            'Development Status :: 5 - Production/Stable', 
-            'Environment :: Console',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Natural Language :: English',
-            'Operating System :: Unix',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Fortran',
-            'Topic :: Scientific/Engineering :: Atmospheric Science',
-        ],
-    )
+    # Write version file
+    with open(fn, 'w') as version_file:
+        version_file.write(version_str.format(VERSION))
+
+# Write version and install
+_write_version_file()
+
+setup(
+    name = "parcel_model",
+    author = "Daniel Rothenberg",
+    author_email = "darothen@mit.edu",
+    maintainer = "Daniel Rothenberg",
+    maintainer_email = "darothen@mit.edu",
+    description = "parcel_model: 0D adiabatic cloud parcel model",
+    long_description = """
+        This code implements a relatively simple, adiabatic cloud parcel model for studying aerosol
+        activation. It allows the user to peform and iterate parcel model experiments in a simple
+        fashion through the use of an object-oriented implementation. Furthermore, interfaces for
+        several numerical solvers are built into the model so that users can choose whatever scheme
+        they would like to run with the model.
+    """,
+    license = "New BSD (3-clause)",
+    url = "https://github.com/darothen/parcel_model",
+    version = VERSION,
+    download_url = "https://github.com/darothen/parcel_model",
+
+    # TODO: Update install requirements and corresponding documentation
+    install_requires = ['numpy', 'scipy', 'pandas', ],
+    packages = ["parcel_model", ],
+    ext_modules = extensions,
+    cmdclass = {'build_ext': build_ext},
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Fortran',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+    ],
+)
