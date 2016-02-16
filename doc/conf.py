263317246baca77739e41f8bb936dd7180a40002
Daniel Rothenberg <darothen@mit.edu>
2016-02-16 14:02:45 -0500

    Removing hard-coded RTD theme path from conf

--- doc/conf.py
+++ doc/conf.py
@@ -136,7 +136,8 @@ html_theme_options = {
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), ]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), ]
+#html_theme_path = []
 
 
 # The name for this set of Sphinx documents.  If None, it defaults to
